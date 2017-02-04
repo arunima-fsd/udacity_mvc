9d7dec37b76cdd3bebf00ed9e7bb3704e222c145
Arunima Srivastava <arunimaasrivastava@gmail.com>
2017-02-04 22:02:19 +0530

    Fixed minor bug. Current cat displays now on page refresh.

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ $(function () {
             if(!localStorage.cats){
                 localStorage.cats = JSON.stringify([]);
             }
+            if(!localStorage.currentCat){
+                localStorage.currentCat = JSON.stringify([]);
+            }
         },
         getAllCats: function () {
             var data = JSON.parse(localStorage.cats);
@@ -29,6 +32,19 @@ $(function () {
             data.push(obj);
             localStorage.cats = JSON.stringify(data);
         },
+        setCurrentCat: function (id) {
+            var data = JSON.parse(localStorage.cats);
+            for(var i = 0; i < data.length; i++){
+                if(data[i].cat_id === id){
+                    localStorage.currentCat = JSON.stringify([data[i]]);
+                    break;
+                }
+            }
+
+        },
+        getCurrentCat: function () {
+            return JSON.parse(localStorage.currentCat)[0];
+        },
         modifyCatCount: function (id, count) {
             var data = JSON.parse(localStorage.cats);
             for(var i = 0; i < data.length; i++){
@@ -38,11 +54,28 @@ $(function () {
                 }
             }
             localStorage.cats = JSON.stringify(data);
+        },
+        modifyCurrCatCount: function (count) {
+          var cat = JSON.parse(localStorage.currentCat);
+          console.log(cat);
+          cat[0].count = count;
+          localStorage.currentCat = JSON.stringify(cat);
+        },
+        modifyCat: function (cat) {
+            var data = JSON.parse(localStorage.cats);
+            for(var i = 0; i < data.length; i++){
+                if(data[i].cat_id === cat.id){
+                    data[i] = cat;
+                    break;
+                }
+            }
+            localStorage.cats = JSON.stringify(data);
+
         }
     };
 
 
-    
+
     var octopus = {
         addNewCat:function (catName) {
             var catId = Math.floor(Math.random() * 100);
@@ -58,17 +91,35 @@ $(function () {
             return model.getAllCats();
 
         },
-        getCat: function(id){
-            return model.getCatObject(id);
+        setCurrentCat:function (id) {
+            model.setCurrentCat(id);
+
+        },
+        getCurrentCat: function () {
+            return model.getCurrentCat();
+
         },
         modifyCount: function (id, count) {
             model.modifyCatCount(id, count);
+        },
+        modifyCurrCatCount: function (count) {
+            model.modifyCurrCatCount(count);
+
+        },
+        modifyCat:function (cat, name, url, count) {
+            cat.cat_name = name;
+            cat.image_url = url;
+            cat.count = count;
+            model.modifyCat(cat);
+            view1.init();
+            view2.render(cat);
 
         },
         init:function () {
             model.init();
             view1.init();
             view2.init();
+            admin_view.init();
 
         }
     };
@@ -79,10 +130,13 @@ $(function () {
             var newCat = $('#cat-form');
             newCat.submit(function(){
                 var catName = $('#cat-name').val();
-                console.log(catName);
                 octopus.addNewCat(catName);
                 $('#cat-name').val("");
+
             });
+            if(octopus.getCurrentCat()){
+                view2.render(octopus.getCurrentCat());
+            }
             view1.render();
         },
         render:function () {
@@ -103,24 +157,56 @@ $(function () {
             var cat_items = $('.cat-items');
             cat_items.click(function () {
                 var id = parseInt((this.id.slice(3)));
-                var cat = octopus.getCat(id);
+                octopus.setCurrentCat(id);
+                var cat = octopus.getCurrentCat();
                 view2.render(cat);
             });
         },
         render: function (cat) {
             var displayArea = $('.cat');
             displayArea.html('<h3> '+ cat.cat_name +'</h3> ' +
-                    '<img class="cat_img_style" src="'+ cat.image_url +'"> <h4>Count:'+ cat.count +'</h4>');
+                    '<img class="cat_img_style" src="'+ cat.image_url +'"> <h4>Count:'+ cat.count +'</h4>' +
+                '<input id="admin-mode" type="button" value="admin">');
+            admin_view.init(cat);
             var cat_img = $('.cat_img_style');
             cat_img.click(function () {
                 cat.count += 1;
                 octopus.modifyCount(cat.cat_id, cat.count);
+                octopus.modifyCurrCatCount(cat.count);
                 $('.cat').find('h4').text('Count: '+ cat.count);
             });
+
 
         }
     };
 
+    var admin_view={
+        init:function (cat) {
+            var admin = $('#admin-mode');
+            var adminDiv = $('#admin');
+            admin.click(function () {
+                adminDiv.toggleClass('hideAdmin');
+
+            });
+            admin_view.render(cat);
+        },
+        render:function (cat) {
+            var cancelBtn = $('#cancelBtn');
+            var saveBtn = $('#saveBtn');
+            cancelBtn.click(function () {
+                $('#admin').toggleClass('hideAdmin');
+            });
+
+            saveBtn.click(function () {
+                var cat_name = $('#catName').val();
+                var image_url = $('#imageUrl').val();
+                var count = $('#clicks').val();
+                octopus.modifyCat(cat, cat_name, image_url, count);
+                $('#admin').toggleClass('hideAdmin');
+            });
+
+        }
+    };
 
     octopus.init();
 });
